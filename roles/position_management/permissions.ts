b7f67a58e4fa964827855cfe59577ce36f42c5c2
Razvan Draghici <tzumby@gmail.com>
2024-06-25 14:16:34 -0400

    Fixup

--- roles/position_management/permissions.ts
+++ roles/position_management/permissions.ts
@@ -4,13 +4,28 @@ import { oneOf, allowErc20Approve } from "../../utils"
 
 export default [
   allowErc20Approve([contracts.arbitrumOne.arb, contracts.arbitrumOne.usdc], [contracts.arbitrumOne.uniswap.positionNft]),
-  allow.arbitrumOne.uniswap.positions_nft.mint({
+  allow.arbitrumOne.uniswap.positionNft.mint({
     token0: contracts.arbitrumOne.arb,
     token1: contracts.arbitrumOne.usdc,
     fee: 500,
     recipient: c.avatar,
   }),
-  allow.arbitrumOne.uniswap.positions_nft.collect({
+  allow.arbitrumOne.uniswap.positionNft.collect({
     recipient: c.avatar
   }),
+  allow.arbitrumOne.uniswap.positionNft.increaseLiquidity(
+    {
+      tokenId: nftIds ? oneOf(nftIds) : c.avatarIsOwnerOfErc721,
+    },
+    {
+      send: true,
+    }
+  ),
+  allow.arbitrumOne.uniswap.positionNft.decreaseLiquidity(
+    nftIds
+      ? {
+          tokenId: oneOf(nftIds),
+        }
+      : undefined
+  )
 ] satisfies Permissions;
